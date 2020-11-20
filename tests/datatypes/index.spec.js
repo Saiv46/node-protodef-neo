72fb018dbce77167fca4ce6c765c3dccd9ef6d0b
Saiv46 <47699394+Saiv46@users.noreply.github.com>
2020-11-20 09:46:23 +0800

    Add 2 more complex tests

--- tests/datatypes/index.spec.js
+++ tests/datatypes/index.spec.js
@@ -73,3 +73,79 @@ testType({
     }
   ]
 })
+
+testType({
+  name: 'complex (switch + countFields)',
+  type: dt.array,
+  value: [
+    { type: 1 },
+    { type: 2, title: 'Welcome' },
+    { type: 3, subtitle: 'to the c*m zone' },
+    { type: 4, id: 65535 }
+  ],
+  bytes: 31,
+  params: {
+    countType: dt.varint,
+    type: [
+      dt.container,
+      [
+        { name: 'type', type: dt.u8 },
+        {
+          anon: true,
+          type: [
+            dt.switch,
+            {
+              compareTo: 'type',
+              fields: {
+                1: dt.void,
+                2: [dt.container, [{ name: 'title', type: dt.cstring }]],
+                3: [dt.container, [{ name: 'subtitle', type: [dt.pstring, { countType: dt.varint }] }]],
+                4: [dt.container, [{ name: 'id', type: dt.u16 }]]
+              }
+            }
+          ]
+        }
+      ]
+    ]
+  }
+})
+
+const recursion = [
+  dt.container,
+  [
+    { name: 'a', type: dt.u8 },
+    {
+      name: 'b',
+      type: [
+        dt.switch,
+        {
+          compareTo: 'a',
+          fields: { 0: dt.cstring }
+        }
+      ]
+    }
+  ]
+]
+recursion[1][1].type[1].default = recursion
+
+const recursionValue = {}
+let recursionBytes = 0
+for (let i = 16, v = recursionValue; i > -1; i--) {
+  v.a = i
+  recursionBytes++
+  if (i) {
+    v.b = {}
+    v = v.b
+  } else {
+    v.b = 'String'
+    recursionBytes += 7
+  }
+}
+
+testType({
+  name: 'complex (recursion)',
+  type: recursion[0],
+  value: recursionValue,
+  bytes: recursionBytes,
+  params: recursion[1]
+})
