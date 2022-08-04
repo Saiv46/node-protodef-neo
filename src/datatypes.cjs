6fdfebe3f0be0c09a772f0fc66b4d65d27390c8b
Alexander Ivanov <saiv46@yandex.ru>
2022-08-05 04:34:07 +0800

    Complete switch to ES modules

--- src/datatypes.cjs
+++
@@ -1,1 +0,0 @@
-module.exports = require('esm')(module)('./datatypes/index.mjs')
--- src/index.cjs
+++
@@ -1,2 +0,0 @@
-const pack = require('esm')(module)('./index.mjs')
-module.exports = Object.assign(pack.default, pack)
